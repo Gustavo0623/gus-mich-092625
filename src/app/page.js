55add4e3fafc72757b0231530d88620c8f5305f0
Gustavo0623 <gustavo.0623@outlook.com>
2025-04-25 02:54:14 -0700

    landing page layout complete

--- src/app/page.js
+++ src/app/page.js
@@ -1,103 +1,29 @@
-import Image from "next/image";
-
 export default function Home() {
   return (
-    <div className="grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]">
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
-        <Image
-          className="dark:invert"
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol className="list-inside list-decimal text-sm/6 text-center sm:text-left font-[family-name:var(--font-geist-mono)]">
-          <li className="mb-2 tracking-[-.01em]">
-            Get started by editing{" "}
-            <code className="bg-black/[.05] dark:bg-white/[.06] px-1 py-0.5 rounded font-[family-name:var(--font-geist-mono)] font-semibold">
-              src/app/page.js
-            </code>
-            .
-          </li>
-          <li className="tracking-[-.01em]">
-            Save and see your changes instantly.
-          </li>
-        </ol>
-
-        <div className="flex gap-4 items-center flex-col sm:flex-row">
-          <a
-            className="rounded-full border border-solid border-transparent transition-colors flex items-center justify-center bg-foreground text-background gap-2 hover:bg-[#383838] dark:hover:bg-[#ccc] font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 sm:w-auto"
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className="dark:invert"
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            className="rounded-full border border-solid border-black/[.08] dark:border-white/[.145] transition-colors flex items-center justify-center hover:bg-[#f2f2f2] dark:hover:bg-[#1a1a1a] hover:border-transparent font-medium text-sm sm:text-base h-10 sm:h-12 px-4 sm:px-5 w-full sm:w-auto md:w-[158px]"
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Read our docs
-          </a>
+    <div className="main py-8 md:py-6 lg:py-4 flex flex-col justify-around items-center h-svh">
+      <div className="text-center title text-5xl font-medium sm:text-6xl md:text-7xl lg:text-8xl" style={{fontFamily: '"Pinyon Script", cursive', color: "#6c416e"}}>Gustavo &amp; Michelle</div>
+      <div className="flex flex-col items-center">
+        <img src="/envelope-seal.PNG" className="w-1/2 sm:w-72 md:w-80 lg:w-96 cursor-pointer hover:drop-shadow-[2px_2px_1px_black]" alt="envlope"/>
+        <p className="pt-4 text-2xl font-bold sm:text-3xl lg:text-4xl" style={{fontFamily: '"Pinyon Script", cursive', color: "#6c416e"}}>September 26, 2025</p>
+      </div>
+      <div className="flex flex-wrap justify-evenly icon-box w-full">
+        <div className="flex flex-col flex-1/2 my-4 md:flex-1/4 md:my-0 justify-end items-center">
+          <p className="text-center font-bold text-lg sm:text-xl lg:text-2xl" style={{fontFamily: '"Pinyon Script", cursive', color: "#6c416e"}}>Wedding Details</p>
+          <img className="border-2 border-[#6c416e] rounded-full w-14 h-auto sm:w-16 md:w-18 lg:w-22 bg-fuchsia-50 cursor-pointer hover:shadow-[2px_2px_2px_black]" src="rings2.svg" alt="rings icon"/>
         </div>
-      </main>
-      <footer className="row-start-3 flex gap-[24px] flex-wrap items-center justify-center">
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          className="flex items-center gap-2 hover:underline hover:underline-offset-4"
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div className="flex flex-col flex-1/2 my-4 md:flex-1/4 md:my-0 justify-end items-center">
+          <p className="text-center font-bold text-lg sm:text-xl lg:text-2xl" style={{fontFamily: '"Pinyon Script", cursive', color: "#6c416e"}}>Wedding Entourage</p>
+          <img className="border-2 border-[#6c416e] rounded-full w-14 h-auto sm:w-16 md:w-18 lg:w-22 bg-fuchsia-50 cursor-pointer hover:shadow-[2px_2px_2px_black]" src="entourage2.svg" alt="entourage icon"/>
+        </div>
+        <div className="flex flex-col flex-1/2 my-4 md:flex-1/4 md:my-0 justify-end items-center">
+          <p className="text-center font-bold text-lg sm:text-xl lg:text-2xl" style={{fontFamily: '"Pinyon Script", cursive', color: "#6c416e"}}>Dress Code</p>
+          <img className="border-2 border-[#6c416e] rounded-full w-14 h-auto sm:w-16 md:w-18 lg:w-22 bg-fuchsia-50 cursor-pointer hover:shadow-[2px_2px_2px_black]" src="dress2.svg" alt="dress icon"/>
+        </div>
+        <div className="flex flex-col flex-1/2 my-4 md:flex-1/4 md:my-0 justify-end items-center">
+          <p className="text-center font-bold text-lg sm:text-xl lg:text-2xl" style={{fontFamily: '"Pinyon Script", cursive', color: "#6c416e"}}>RSVP</p>
+          <img className="border-2 border-[#6c416e] rounded-full w-14 h-auto sm:w-16 md:w-18 lg:w-22 bg-fuchsia-50 cursor-pointer hover:shadow-[2px_2px_2px_black]" src="rsvp.svg" alt="RSVP icon"/>
+        </div>
+      </div>
     </div>
   );
 }
