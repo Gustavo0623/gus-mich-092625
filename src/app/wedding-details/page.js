11ded316cdf6ce6e762be709ed18c97fc7335b18
Gustavo0623 <gustavo.0623@outlook.com>
2025-04-29 19:54:23 -0700

    adjust font and styling of details page

--- src/app/wedding-details/page.js
+++ src/app/wedding-details/page.js
@@ -1,7 +1,10 @@
 export default function Details() {
     return (
-        <div className="flex flex-col items-center h-screen justify-center">
+        <div className="flex flex-wrap items-center h-screen justify-center">
             <img className="max-h-screen" src="/invite.JPG" alt="invitation"/>
+            <div className="flex-1/2 min-h-screen">
+                <p className="text-xl md:text-2xl lg:text-4xl font-semibold text-center" style={{color: "#6c416e"}} >Location</p>
+            </div>
         </div>
     )
 }
