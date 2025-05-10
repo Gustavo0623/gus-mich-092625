e5f77c97fae6bb4c61281eefc6951307164436ad
Gustavo0623 <gustavo.0623@outlook.com>
2025-05-10 15:22:08 -0700

    update dress code page

--- src/app/dress-code/page.js
+++ src/app/dress-code/page.js
@@ -2,17 +2,17 @@ import Link from "next/link"
 
 export default function DressCode() {
     return (
-        <div className="flex flex-wrap items-center justify-center p-6 gap-6">
+        <div className="flex flex-wrap items-center justify-center">
             {/* Return Home Link */}
             <div className="w-full">
                 <Link
                 href="/"
-                className="text-white bg-opacity-50 px-4 py-2 rounded hover:bg-opacity-75 transition" style={{backgroundColor: "#6c416e"}}
+                className="absolute px-4 py-2 rounded" style={{color: "#6c416e"}}
                 >
                     ←
                 </Link>
             </div>
-            <img className="max-h-screen" src="/attire.JPG" alt="attire guidelines"/>
+            <img className="h-dvh" src="/attire.JPG" alt="attire guidelines"/>
         </div>
     )
 }
